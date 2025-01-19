7ce9f691a4af6553b7b9c3c5e515d60df5466864
july-es-pino <julia.espinosa.garcia@gmail.com>
2025-01-19 16:26:13 -0600

    Ejercicio-02-alura

---
+++ app.js
@@ -0,0 +1,53 @@
+/*Pregunta al usuario qué día de la semana es. 
+Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!".
+De lo contrario, muestra "¡Buena semana!".*/
+
+/*let preguntaUsuario = prompt ('¿Qué dia de la semana es?');
+let diaUno = ('sabado');
+let diaDos = ('domingo');
+
+if (preguntaUsuario === diaUno || diaDos === preguntaUsuario){
+    alert('¡Buen fin de semana!')
+
+}else{
+    alert('¡Linda Semana!')
+};*/
+
+
+/*Ejercicio2
+Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.*/
+
+/*let numeroUsuario = prompt('Ingresa un numero de -10 a 10');
+if( numeroUsuario >0)  {
+    alert ('El numero que ingresaste es ¡Positivo!')
+
+}else{
+    alert ('El número que ingresaste es ¡Negativo!')
+};*/
+
+
+/*Crea un sistema de puntuación para un juego. 
+Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". 
+En caso contrario, muestra "Intentalo nuevamente para ganar."*/
+
+/*let puntuacionUsuario = prompt ('¿Cúal fue tu puntuación?');
+if (puntuacionUsuario >= 100){
+    alert ('¡Felicidades, has ganado!')
+
+} else{
+    alert ('Intentalo nuevamente')
+};*/
+
+
+/*Crea un mensaje que informe al usuario sobre el saldo de su cuenta, 
+utilizando un template string para incluir el valor del saldo.*/
+
+/*let  saldo = 1500;
+alert(`El saldo de tu cuenta es: ${saldo}`);*/
+
+
+/*Pide al usuario que ingrese su nombre mediante un prompt. 
+Luego, muestra una alerta de bienvenida usando ese nombre.*/
+
+let nombreUsuario = prompt('Porfavor ingresa tu nombre');
+alert('Bienvenidx '+ nombreUsuario);
